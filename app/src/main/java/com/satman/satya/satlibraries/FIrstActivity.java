d6fcb28a3040ee484b53ba74b4f6d4239f2b6ed6
satya <satya.sampathirao@gmail.com>
2018-04-08 08:31:24 +0530

    Initial commit

---
+++ app/src/main/java/com/satman/satya/satlibraries/FIrstActivity.java
@@ -0,0 +1,28 @@
+package com.satman.satya.satlibraries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.satman.satya.arthematic.Operations;
+
+public class FIrstActivity extends AppCompatActivity {
+
+
+    Operations op ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        op = new Operations();
+
+        int sum = op.add(10,20);
+        
+
+    }
+
+}
+
+
+
---
+++ arthematic/src/main/java/com/satman/satya/arthematic/Operations.java
@@ -0,0 +1,34 @@
+package com.satman.satya.arthematic;
+
+/**
+ * Created by satya on 08-04-2018.
+ */
+
+public class Operations {
+
+    public int add(int x, int y){
+
+        return x+y;
+    }
+
+
+    public int subtract(int x, int y){
+
+        return x-y;
+    }
+
+
+    public int multiply(int x, int y){
+
+        return x*y;
+    }
+
+
+    public int division(int x, int y){
+
+        return x/y;
+    }
+
+
+
+}
